ee30e88153bb8593e1d9078e2307cd74cdbd4827
arthurramires <arthurramires1802@outlook.com>
2020-07-03 09:18:23 -0400

    Implementando testes

---
+++ src/__tests__/pages/Signin.spec.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from 'react-native-testing-library';
+import SignIn from '../../pages/SignIn';
+
+jest.mock('@react-navigation/native', () => {
+    return {
+        useNavigation: jest.fn(),
+    };
+});
+
+describe('Signin Page', () => {
+    it('Should contains email/password', () => {
+        const { getByPlaceholder } = render(<SignIn />);
+
+        expect(getByPlaceholder('E-mail')).toBeTruthy();
+        expect(getByPlaceholder('Senha')).toBeTruthy();
+    });
+});
